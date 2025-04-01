6cc9ea63620ba02021bf16ce913984f23ede6c62
Rufus770 <rufusk770@gmail.com>
2025-04-01 10:33:57 +0530

    project completed

--- app.js
+++ app.js
@@ -14,7 +14,9 @@ app.use(cors());
 app.use(morgan('dev'));
 app.use(express.json());
 
-mongoose.connect('mongodb://127.0.0.1:27017/kleEcom')
+
+let MONGODB_URL = 'mongodb+srv://komalladwa5:qDPKwYtoEYD6sKYI@cluster0.d2uvvqc.mongodb.net/?retryWrites=true&w=majority'
+mongoose.connect(MONGODB_URL)
 .then(()=>{
     console.log('db connected');
 }).catch((err)=>{
